e73f721182452b4af367b3231cbde8ad868ac945
Jeremy Dilatush <jtd2004a@sbcglobal.net>
2020-10-17 16:02:30 -0700

    do a little more in sampler2.sh

--- sampler2.sh
+++ sampler2.sh
@@ -35,10 +35,10 @@ sudo tcpdump -ni$iface -s2048 -w$outfile ip proto 2 or ip6 proto 0 &
 tp=$!
 echo "tcpdump pid='$tp'"
 
-doit $m4base.7 -E - $u4base.1 $u4base.1,$u4base.2 $u4base.3 -
-doit $s4base.8 -I - $u4base.4 $u4base.4,$u4base.5 $u4base.6 -
-doit $m6base:7 -E - $u6base:1 $u6base:1,$u6base:2 $u6base:3 -
-doit $s6base:8 -I - $u6base:4 $u6base:4,$u6base:5 $u6base:6 -
+doit $m4base.7 -E - $u4base.1 $u4base.1,$u4base.2 $u4base.3 - $u4base.7
+doit $s4base.8 -I - $u4base.4 $u4base.4,$u4base.5 $u4base.6 - $u4base.8
+doit $m6base:7 -E - $u6base:1 $u6base:1,$u6base:2 $u6base:3 - $u6base:7
+doit $s6base:8 -I - $u6base:4 $u6base:4,$u6base:5 $u6base:6 - $u6base:8
 
 echo "Terminate this program with control-C"
 # Somehow "sudo kill $tp" isn't working in a similar script, which is why 
