0750c785b95ba7de2f29f8fdcc631a3cce35e36c
Jeremy Dilatush <jtd2004a@sbcglobal.net>
2022-03-28 15:52:55 -0700

    include the license text in the compiled binary; display with -vvh

--- oligocast.c
+++ oligocast.c
@@ -1,29 +1,29 @@
-/*
- * oligocast.c
- * Copyright (c) 2020 Jeremy Dilatush
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY JEREMY DILATUSH AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL JEREMY DILATUSH OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
+static const char License[] =
+"oligocast.c                                                                \n"
+"Copyright (c) 2020 Jeremy Dilatush                                         \n"
+"All rights reserved.                                                       \n"
+"                                                                           \n"
+"Redistribution and use in source and binary forms, with or without         \n"
+"modification, are permitted provided that the following conditions         \n"
+"are met:                                                                   \n"
+"1. Redistributions of source code must retain the above copyright          \n"
+"   notice, this list of conditions and the following disclaimer.           \n"
+"2. Redistributions in binary form must reproduce the above copyright       \n"
+"   notice, this list of conditions and the following disclaimer in the     \n"
+"   documentation and/or other materials provided with the distribution.    \n"
+"                                                                           \n"
+"THIS SOFTWARE IS PROVIDED BY JEREMY DILATUSH AND CONTRIBUTORS              \n"
+"``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED\n"
+"TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR \n"
+"PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL JEREMY DILATUSH OR CONTRIBUTORS \n"
+"BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR     \n"
+"CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF       \n"
+"SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS   \n"
+"INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN    \n"
+"CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)    \n"
+"ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE \n"
+"POSSIBILITY OF SUCH DAMAGE.                                                \n"
+"\n";
 
 /*
  * "oligocast" is a test program for IP multicast (IPv4 and IPv6).  It's
@@ -537,6 +537,9 @@ static enum command_action option(struct config *cfg, int pc, int oc, char *arg)
         return(command_action_none);
 
     case 'h': /* display help (usage) message */
+        if (cfg->cfg_verbose > 1) {
+            fputs(License, stderr);
+        }
         if (pc == 0) {
             /* on command line: display and exit */
             usage();
